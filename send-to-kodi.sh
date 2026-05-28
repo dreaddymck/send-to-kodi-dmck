fadc14262223fae93042c61c2b5e11fda931d74e
dreaddymck <dreaddymck@users.noreply.github.com>
2026-05-28 12:11:24 -0400

    fixed download race condition, misc changes

--- send-to-kodi.sh
+++ send-to-kodi.sh
@@ -10,6 +10,7 @@ SEND_TO_KODI_TMP=$(dirname $(mktemp -u))
 SEND_TO_KODI_PWD=$(pwd)
 SEND_TO_KODI_DIR="$(dirname "$(readlink -f "$0")")"
 SEND_TO_KODI_HISTORY=$HOME/.config/send_to_kodi/.send_to_kodi_history
+
 source "$SEND_TO_KODI_DIR/lib/maintenance"
 source "$SEND_TO_KODI_DIR/lib/requirements"
 source "$SEND_TO_KODI_DIR/lib/config"
@@ -34,13 +35,16 @@ fi
 
 # Load ytdl environment,
 # Bypass $REMOTE check
-# Default to tmp directory
+# Default to isolated tmp directory to prevent race conditions with concurrent instances
 if [[ "$INPUT" =~ ^(dlrz|dl|rz)$ ]]; then
     unset INPUT
-    cd "$SEND_TO_KODI_TMP" && ytdl_dlrz
+    export JOB_DIR
+    JOB_DIR=$(mktemp -d "$SEND_TO_KODI_TMP/stk-XXXXXX")
+    cd "$JOB_DIR" && ytdl_dlrz
     exit
 fi
 
 [[ $REMOTE ]] || error "Remote address NOT specified, see --help"
+
 send_to_kodi_banner
 main
